48b6bee9fe0f5b7056c9ad7a160a68d5b6b89f2d
Aaron Stephenson <aaron@bronron.com>
2015-03-24 12:00:42 +1100

    Initial commit

---
+++ Calendar WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Calendar WatchKit Extension
+//
+//  Created by Aaron Stephenson on 24/03/2015.
+//  Copyright (c) 2015 Bronron Apps. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
---
+++ Calendar WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Calendar WatchKit Extension
+//
+//  Created by Aaron Stephenson on 24/03/2015.
+//  Copyright (c) 2015 Bronron Apps. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
